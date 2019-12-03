7969caa5eaba5d544a0317e709c9f50a81f22c28
alsh <shpirkov@gmail.com>
2019-12-03 17:02:26 +0200

    add manifest context replacement

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,21 @@
         port: 3000,
         https: true,
         overlay: {
-            warnings: false,
+            warnings: true,
             errors: true
         }
+    },
+    chainWebpack: config => {
+        config.plugin('copy').tap(args => {
+            args[0].push({ // to add rule
+                    from: 'src/manifest.xml',
+                    transform: function(content, path) {
+                        var find = 'localhost:3000'
+                        var replacement = 'rooms-258209.appspot.com'
+                        return Promise.resolve(content.toString().split(find).join(replacement));
+                    }
+                })
+            return args;
+        })
     }
-}
-;
+};
