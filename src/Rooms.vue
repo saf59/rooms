ef7aeb3663b8dd4bdcad4ffb94c38c1b8020abb4
alsh <shpirkov@gmail.com>
2019-12-04 13:52:56 +0200

    data via axios and external request

--- src/Rooms.vue
+++ src/Rooms.vue
@@ -15,24 +15,27 @@
 
 <script>
     import grid from './components/Grid.vue'
-
+    import axios from 'axios';
     export default {
-        name: 'demo',
         components: {
             grid
         },
         data:  () => {
             return {
                 searchQuery: '',
-                selectedInfo: 'Our service has selected 4 meeting rooms for you.',
-                gridColumns: ['rate', 'name', 'capacity'],
-                gridData: [
-                    {rate: 97, name: 'Library, 2nd floor, Hall 3', capacity: 'up to 20'},
-                    {rate: 63, name: 'Main building, 4th floor, room 5', capacity: 'up to 15'},
-                    {rate: 22, name: 'Laboratory building, penthouse', capacity: 'up to 50'},
-                    {rate: 11, name: 'Lobby, interview room', capacity: 'up to 10'}
-                ]
+                selectedInfo: '',
+                gridColumns: [],
+                gridData: []
             }
+        },
+        mounted() {
+            axios
+                .get('/goodRooms.json')
+                .then(response => {
+                    this.gridData = response.data;
+                    this.gridColumns = Object.keys(this.gridData[0]);
+                    this.selectedInfo = 'Our service has selected '+this.gridData.length+' meeting rooms for you.';
+                });
         }
     }
 </script>
